b9801d302c315ae698f6a353344736b84573c4be
unknown <mazepamalina@gmail.com>
2020-10-06 21:53:14 +0200

    Stworzenie 1 modelu oraz zainicjowanie bazy danych za pomocą EF Core

---
+++ Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Portfolio.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+            public DbSet<Projects> Projects { get; set; }
+    }
+}
+
+
+
